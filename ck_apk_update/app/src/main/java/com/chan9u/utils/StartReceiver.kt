9dbcf70bf5f5c741d2f5f15cf09f09b25802df3e
chan9You <41310752+kyuchanE@users.noreply.github.com>
2021-05-18 18:01:29 +0900

    21.05.18 디바이스 실행시 자동 실행

---
+++ ck_apk_update/app/src/main/java/com/chan9u/utils/StartReceiver.kt
@@ -0,0 +1,22 @@
+package com.chan9u.utils
+
+import android.content.BroadcastReceiver
+import android.content.Context
+import android.content.Intent
+import android.os.Build
+import android.util.Log
+
+class StartReceiver: BroadcastReceiver() {
+
+    override fun onReceive(context: Context?, intent: Intent?) {
+        Log.d("@@@@@@@@@", "StartReceiver >> onReceive ")
+        context?.let {
+            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+                context.startForegroundService(Intent(context, AutoUpdateService::class.java))
+            } else {
+                context.startService(Intent(context, AutoUpdateService::class.java))
+            }
+        }
+    }
+
+}
